508ec3f2fe2309676e53323762a5c653eedfc014
youness <younesserbai@gmail.com>
2024-04-28 20:26:16 +0100

    Implement client-side form validation for signin

--- resources/views/aut/sign-in.blade.php
+++ resources/views/aut/sign-in.blade.php
@@ -1,45 +1,74 @@
 @extends('aut.layout')
 @section('content')
-    <form action="{{ route('auth_Login') }}" method="POST" id="loginForm" role="form">
-        @csrf
-        <label>Email</label>
-        <div class="mb-3">
-            <input type="email" class="form-control" placeholder="Email" name="email" id="email" aria-label="Email"
-                aria-describedby="email-addon">
-            <div id="emailError" class="text-danger">
-                @error('email')
-                    <div class="text-danger">{{ $message }}</div> <!-- Display error message -->
-                @enderror
-            </div> <!-- Error message placeholder -->
+    <section>
+        <div class="page-header min-vh-75">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                        <div class="card card-plain mt-8">
+                            <div class="card-header pb-0 text-left bg-transparent">
+                                <h3 class="font-weight-bolder text-info text-gradient"> Login</h3>
+                                <p class="mb-0">Enter your email and password</p>
+                            </div>
+                            <div class="card-body">
+                                <form action="{{ route('auth_Login') }}" method="POST" id="loginForm" role="form">
+                                    @csrf
+                                    <label>Email</label>
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" placeholder="Email" name="email"
+                                            id="email" aria-label="Email" aria-describedby="email-addon">
+                                        <div id="emailError" class="text-danger">
+                                            @error('email')
+                                                <div class="text-danger">{{ $message }}</div>
+                                                <!-- Display error message -->
+                                            @enderror
+                                        </div> <!-- Error message placeholder -->
+                                    </div>
+                                    <label>Password</label>
+                                    <div class="mb-3">
+                                        <input type="password" class="form-control" placeholder="Password" name="password"
+                                            id="password" aria-label="Password" aria-describedby="password-addon">
+                                        <div id="passwordError" class="text-danger"> @error('password')
+                                                <div class="text-danger">{{ $message }}</div>
+                                                <!-- Display error message -->
+                                            @enderror
+                                        </div> <!-- Error message placeholder -->
+                                    </div>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="rememberMe" checked>
+                                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                                    </div>
+                                </form>
+                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                    <p class="mb-4 text-sm mx-auto">
+                                        Don't have an account?
+                                        <a href="{{ route('auth_getRogister') }}"
+                                            class="text-info text-gradient font-weight-bold">Sign
+                                            up</a>
+                                    <p class="mb-4 text-sm mx-auto">
+                                        Forgot your password?
+                                        <a href="{{ route('auth_forgetPassword') }}"
+                                            class="text-info text-gradient font-weight-bold">Forgot Password
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                            <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
+                                style="background-image:url('https://th.bing.com/th/id/OIP.DDYGhFqUNR5rc1TtT2Vh-wHaE8?w=281&h=187&c=7&r=0&o=5&dpr=1.5&pid=1.7')">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <label>Password</label>
-        <div class="mb-3">
-            <input type="password" class="form-control" placeholder="Password" name="password" id="password"
-                aria-label="Password" aria-describedby="password-addon">
-            <div id="passwordError" class="text-danger"> @error('password')
-                    <div class="text-danger">{{ $message }}</div> <!-- Display error message -->
-                @enderror
-            </div> <!-- Error message placeholder -->
-        </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" id="rememberMe" checked>
-            <label class="form-check-label" for="rememberMe">Remember me</label>
-        </div>
-        <div class="text-center">
-            <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
-        </div>
-    </form>
-    <div class="card-footer text-center pt-0 px-lg-2 px-1">
-        <p class="mb-4 text-sm mx-auto">
-            Don't have an account?
-            <a href="{{ route('auth_getRogister') }}" class="text-info text-gradient font-weight-bold">Sign
-                up</a>
-        <p class="mb-4 text-sm mx-auto">
-            Forgot your password?
-            <a href="{{ route('auth_forgetPassword') }}" class="text-info text-gradient font-weight-bold">Forgot Password
-            </a>
-        </p>
-    </div>
+    </section>
     <script>
         document.getElementById('loginForm').addEventListener('submit', function(event) {
             if (!validateForm()) {
